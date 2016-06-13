b216bea6adf2ce001307349c9b082076b53839b2
Jason Martocci <martoccijason@gmail.com>
2016-06-13 11:01:39 -0400

    removed shop controller

--- controllers/projectX_controller.js
+++ controllers/projectX_controller.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var _ = require('underscore');
 var router = express.Router();
-var shop = require('../models/shop.js');
+var projectX = require('../models/projectX.js');
 
 router.get('/', function(req,res) {
 	var hbsObject = {logged_in: req.session.logged_in, isUser: req.session.isUser, isAdmin: req.session.isAdmin}
@@ -14,21 +14,21 @@ router.get('/index', function(req,res) {
 });
 
 router.get('/admin', function(req,res) {
-	shop.allUsers(function(data){
+	projectX.allUsers(function(data){
 		var hbsObject = {users : data, logged_in: req.session.logged_in, isUser: req.session.isUser, isAdmin: req.session.isAdmin}
 		res.render('admin', hbsObject);
 	});
 });
 
 router.get('/users', function(req,res) {
-	shop.allUsers(function(data){
+	projectX.allUsers(function(data){
 		var hbsObject = {users : data, logged_in: req.session.logged_in, isUser: req.session.isUser, isAdmin: req.session.isAdmin}
 		res.render('users', hbsObject);
 	});
 });
 
 router.post('/users/createNewUser', function(req,res) {
-	shop.createUser(['userName', 'name', 'emailAddress', 'password', 'role'], [req.body.username, req.body.name, req.body.emailAddress, req.body.password, req.body.role], function(data){
+	projectX.createUser(['userName', 'name', 'emailAddress', 'password', 'role'], [req.body.username, req.body.name, req.body.emailAddress, req.body.password, req.body.role], function(data){
 		res.redirect('/users')
 	});
 });
@@ -36,7 +36,7 @@ router.post('/users/createNewUser', function(req,res) {
 router.delete('/users/delete/:userId', function(req,res) {
 	var condition = 'userId = ' + req.params.userId;
 	console.log('condition', condition);
-	shop.deleteUser(condition, function(data){
+	projectX.deleteUser(condition, function(data){
 		res.redirect('/users')
 	});
 });
@@ -44,7 +44,7 @@ router.delete('/users/delete/:userId', function(req,res) {
 router.put('/users/update/:userId', function(req,res) {
 	var condition = 'userId = ' + req.params.userId;
 	console.log('condition', condition);
-	shop.updateUser({'userName ' : req.body.username, ', name ' : req.body.name, ', emailAddress ' : req.body.emailAddress, ', role ' : req.body.role}, condition, function(data){
+	projectX.updateUser({'userName ' : req.body.username, ', name ' : req.body.name, ', emailAddress ' : req.body.emailAddress, ', role ' : req.body.role}, condition, function(data){
 		res.redirect('/users');
 	});
 });
@@ -55,14 +55,14 @@ router.get('/characters', function(req,res) {
 });
 
 router.get('/heroes', function(req,res) {
-	shop.allHeroes(function(data){
+	projectX.allHeroes(function(data){
 		var hbsObject = {heroes : data, logged_in: req.session.logged_in, isUser: req.session.isUser, isAdmin: req.session.isAdmin}
 		res.render('heroes', hbsObject);
 	});
 });
 
 router.post('/heroes/createNewHeroes', function(req,res) {
-	shop.createHeroes(['plantTypes', 'asset', 'cost', 'energy', 'isSunProducer', 'isShooter', 'isExploding', 'sunFrequency', 'shootingFrequency', 'damage'], [req.body.plantTypes, req.body.asset, req.body.cost, req.body.energy, req.body.isSunProducer, req.body.isShooter, req.body.isExploding, req.body.sunFrequency, req.body.shootingFrequency, req.body.damage], function(data){
+	projectX.createHeroes(['plantTypes', 'asset', 'cost', 'energy', 'isSunProducer', 'isShooter', 'isExploding', 'sunFrequency', 'shootingFrequency', 'damage'], [req.body.plantTypes, req.body.asset, req.body.cost, req.body.energy, req.body.isSunProducer, req.body.isShooter, req.body.isExploding, req.body.sunFrequency, req.body.shootingFrequency, req.body.damage], function(data){
 		res.redirect('/heroes')
 	});
 });
@@ -70,7 +70,7 @@ router.post('/heroes/createNewHeroes', function(req,res) {
 router.delete('/heroes/delete/:heroesId', function(req,res) {
 	var condition = 'heroesId = ' + req.params.heroesId;
 	console.log('condition', condition);
-	shop.deleteHeroes(condition, function(data){
+	projectX.deleteHeroes(condition, function(data){
 		res.redirect('/heroes')
 	});
 });
@@ -78,19 +78,19 @@ router.delete('/heroes/delete/:heroesId', function(req,res) {
 router.put('/heroes/update/:heroesId', function(req,res) {
 	var condition = 'heroesId = ' + req.params.heroesId;
 	console.log('condition', condition);
-	shop.updateHeroes({'plantTypes ' : req.body.plantTypes, ', asset ' : req.body.asset, ', cost ' : req.body.cost, ', energy ' : req.body.energy, ', isSunProducer ' : req.body.isSunProducer, ', isShooter ' : req.body.isShooter, ', isExploding ' : req.body.isExploding, ', sunFrequency ' : req.body.sunFrequency, ', shootingFrequency ' : req.body.shootingFrequency, ', damage ' : req.body.damage}, condition, function(data){
+	projectX.updateHeroes({'plantTypes ' : req.body.plantTypes, ', asset ' : req.body.asset, ', cost ' : req.body.cost, ', energy ' : req.body.energy, ', isSunProducer ' : req.body.isSunProducer, ', isShooter ' : req.body.isShooter, ', isExploding ' : req.body.isExploding, ', sunFrequency ' : req.body.sunFrequency, ', shootingFrequency ' : req.body.shootingFrequency, ', damage ' : req.body.damage}, condition, function(data){
 		res.redirect('/heroes');
 	});
 });
 router.get('/enemies', function(req,res) {
-	shop.allEnemies(function(data){
+	projectX.allEnemies(function(data){
 		var hbsObject = {enemies : data, logged_in: req.session.logged_in, isUser: req.session.isUser, isAdmin: req.session.isAdmin}
 		res.render('enemies', hbsObject);
 	});
 });
 
 router.post('/enemies/createNewEnemies', function(req,res) {
-	shop.createEnemies(['zombieTypes', 'asset', 'damage', 'vx', 'energy'], [req.body.zombieTypes, req.body.asset, req.body.damage, req.body.vx, req.body.energy], function(data){
+	projectX.createEnemies(['zombieTypes', 'asset', 'damage', 'vx', 'energy'], [req.body.zombieTypes, req.body.asset, req.body.damage, req.body.vx, req.body.energy], function(data){
 		res.redirect('/enemies')
 	});
 });
@@ -98,7 +98,7 @@ router.post('/enemies/createNewEnemies', function(req,res) {
 router.delete('/enemies/delete/:enemiesId', function(req,res) {
 	var condition = 'enemiesId = ' + req.params.enemiesId;
 	console.log('condition', condition);
-	shop.deleteEnemies(condition, function(data){
+	projectX.deleteEnemies(condition, function(data){
 		res.redirect('/enemies')
 	});
 });
@@ -106,7 +106,7 @@ router.delete('/enemies/delete/:enemiesId', function(req,res) {
 router.put('/enemies/update/:enemiesId', function(req,res) {
 	var condition = 'enemiesId = ' + req.params.enemiesId;
 	console.log('condition', condition);
-	shop.updateEnemies({'zombieTypes ' : req.body.zombieTypes, ', asset ' : req.body.asset, ', damage ' : req.body.damage, ', vx ' : req.body.vx, ', energy ' : req.body.energy}, condition, function(data){
+	projectX.updateEnemies({'zombieTypes ' : req.body.zombieTypes, ', asset ' : req.body.asset, ', damage ' : req.body.damage, ', vx ' : req.body.vx, ', energy ' : req.body.energy}, condition, function(data){
 		res.redirect('/enemies');
 	});
 });
--- models/projectX.js
+++ models/projectX.js
@@ -1,6 +1,12 @@
 var orm = require('../config/orm.js');
 
 var projectX = {
+	all: function(cb) {
+		orm.all('inventory', function(res){
+			cb(res);
+		});
+	},
+
 	allUsers: function(cb) {
 		orm.all('users', function(res){
 			cb(res);
@@ -21,6 +27,54 @@ var projectX = {
 
 	deleteUser: function(condition, cb) {
 		orm.delete('users', condition, function(res){
+			cb(res);
+		});
+	},
+
+	allHeroes: function(cb) {
+		orm.all('heroes', function(res){
+			cb(res);
+		});
+	},
+
+	createHeroes: function(cols, vals, cb) {
+		orm.createHeroes('heroes', cols, vals, function(res){
+			cb(res);
+		});
+	},
+
+	updateHeroes: function(objColVals, condition, cb) {
+		orm.update('heroes', objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+
+	deleteHeroes: function(condition, cb) {
+		orm.delete('heroes', condition, function(res){
+			cb(res);
+		});
+	},
+
+	allEnemies: function(cb) {
+		orm.all('enemies', function(res){
+			cb(res);
+		});
+	},
+
+	createEnemies: function(cols, vals, cb) {
+		orm.createEnemies('enemies', cols, vals, function(res){
+			cb(res);
+		});
+	},
+
+	updateEnemies: function(objColVals, condition, cb) {
+		orm.update('enemies', objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+
+	deleteEnemies: function(condition, cb) {
+		orm.delete('enemies', condition, function(res){
 			cb(res);
 		});
 	},
