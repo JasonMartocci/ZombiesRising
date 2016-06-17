aed157f33573a5f8c64487c4bf2162181176518c
Jason Martocci <martoccijason@gmail.com>
2016-06-17 13:57:18 -0400

    fixing heroku

--- public/assets/javascript/startGame.js
+++ public/assets/javascript/startGame.js
@@ -70,9 +70,18 @@ $.ajax({
         var characterAssets = heroesData[i].asset + ', ';
         console.log(characterAssets);
         //load assets
-        Q.load(characterAssets + '/assets/audio/ZombiesOnYourLawn.mp3, /assets/images/scoreBoard.png, /assets/audio/Homer_Mmmm_donuts.mp3, /assets/images/background.png, /assets/images/buttonBg1.png, /assets/images/sun.png, /assets/images/bullet.png, /assets/data/level1.json, /assets/data/level2.json, /assets/audio/boom.mp3, /assets/audio/hit.mp3');
+        Q.load(characterAssets + '/assets/audio/ZombiesOnYourLawn.mp3, /assets/images/scoreBoard.png, /assets/audio/Homer_Mmmm_donuts.mp3, /assets/images/background.png, /assets/images/buttonBg1.png, /assets/images/sun.png, /assets/images/bullet.png, /assets/data/level1.json, /assets/data/level2.json, /assets/audio/boom.mp3, /assets/audio/hit.mp3', function() {
+            Q.state.reset({sun: 200, currentPlant: null});
+            Q.stageScene('level', {levelData: Q.assets['/assets/data/level1.json']});  
+            Q.stageScene("sun",1);                
+        },
+        {
+          progressCallback: function(loaded,total) {
+            var element = document.getElementById("loading_progress");
+            element.style.width = Math.floor(loaded/total*100) + "%";
+          }
+        });
       }
     }
   });
-console.log(Q.load);
 });
