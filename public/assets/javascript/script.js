065fd8e7cd9d41402835299231a14dd9f23e81f1
Jason Martocci <martoccijason@gmail.com>
2016-06-27 13:54:30 -0400

    cleaned up admin pages for characters

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -13,4 +13,81 @@ $(document).ready(function() {
       $("#sign_in").show();
       $("#register").hide();
   });
+
+  var _URL = window.URL || window.webkitURL;
+
+  $("#heroes-file-input").change(function(e) {
+      var file, img;
+      if ((file = this.files[0])) {
+          img = new Image();
+          img.onload = function() {
+              if (this.height > 125){
+                swal("Oops...", "Your image is too tall, max height 125px please.", "error");
+                // alert("This is to tall");
+                $('#heroes-file-input').val('');
+              }else{
+              }
+          };
+          img.onerror = function() {
+              alert( "not a valid file: " + file.type);
+          };
+          img.src = _URL.createObjectURL(file);
+      }
+  });
+  $("#newHeroes-file-input").change(function(e) {
+      var file, img;
+      if ((file = this.files[0])) {
+          img = new Image();
+          img.onload = function() {
+              if (this.height > 125){
+                swal("Oops...", "Your image is too tall, max height 125px please.", "error");
+                // alert("This is to tall");
+                $('#newHeroes-file-input').val('');
+              }else{
+              }
+          };
+          img.onerror = function() {
+              alert( "not a valid file: " + file.type);
+          };
+          img.src = _URL.createObjectURL(file);
+      }
+  });
+
+  $("#enemies-file-input").change(function(e) {
+      var file, img;
+      if ((file = this.files[0])) {
+          img = new Image();
+          img.onload = function() {
+              if (this.height > 224){
+                swal("Oops...", "Your image is too tall, max height 224px please.", "error");
+                // alert("This is to tall");
+                $('#enemies-file-input').val('');
+              }else{
+              }
+          };
+          img.onerror = function() {
+              alert( "not a valid file: " + file.type);
+          };
+          img.src = _URL.createObjectURL(file);
+      }
+  });
+
+  $("#newEnemies-file-input").change(function(e) {
+      var file, img;
+      if ((file = this.files[0])) {
+          img = new Image();
+          img.onload = function() {
+              if (this.height > 224){
+                swal("Oops...", "Your image is too tall, max height 224px please.", "error");
+                // alert("This is to tall");
+                $('#newEnemies-file-input').val('');
+              }else{
+              }
+          };
+          img.onerror = function() {
+              alert( "not a valid file: " + file.type);
+          };
+          img.src = _URL.createObjectURL(file);
+      }
+  });
 });
