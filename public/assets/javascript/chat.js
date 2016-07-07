c70ed5cf0db8dd8df791075b22853abe49243664
Jason Martocci <martoccijason@gmail.com>
2016-07-07 11:44:15 -0400

    message board working time to style it

--- public/assets/javascript/chat.js
+++ public/assets/javascript/chat.js
@@ -73,65 +73,66 @@ $(document).ready(function(Q) {
   // Chat feature
   var rootRef = new Firebase('https://vivid-torch-7282.firebaseio.com/chat');
 
-// // delete record when a tag is clicked
-// jQuery('body').on('click', 'a', function() {
-//    var $rec = $(this).closest('[data-id]');
-//     var id = $rec.attr('data-id') || null;
-//     if( id ) {
-//        // remove any nested children
-//        $rec.find('[data-id]').each(function() {
-//           rootRef.child($(this).attr('data-id')).remove(); 
-//        });
+  // delete record when a tag is clicked
+  $('body').on('click', '.deleteThis', function() {
+   var $rec = $(this).closest('[data-id]');
+    var id = $rec.attr('data-id') || null;
+    if( id ) {
+       // remove any nested children
+       $rec.find('[data-id]').each(function() {
+          rootRef.child($(this).attr('data-id')).remove(); 
+       });
        
-//        // remove the record
-//        rootRef.child(id).remove();
-//     }
-//     return false;
-// });
+       // remove the record
+       rootRef.child(id).remove();
+    }
+    return false;
+  });
 
-// add new records at the appropriate level when a button is clicked
-jQuery('body').on('click', 'button', function () {
+  // add new records at the appropriate level when a button is clicked
+  $('body').on('click', 'button', function () {
     var $input = $(this).prev('input');
+    var name = $('#nameInput').val();
     var title = $input.val();
     var parent = $input.closest('[data-id]').attr('data-id') || null;
-    console.log('creating', parent, title);
+    console.log('creating', parent, title, name);
     if (title) {
-        rootRef.push({ 'title': title, 'parent': parent });
+        rootRef.push({ 'name': name, 'title': title, 'parent': parent });
     }
     $input.val('');
     return false;
-})
+  })
 
-rootRef.on('child_added', function (snapshot) {
+  rootRef.on('child_added', function (snapshot) {
     var message = snapshot.val();
     console.log('child_added', message);
-    displayTitleMessage(snapshot.key(), message.title, message.parent);
-});
+    displayTitleMessage(snapshot.key(), message.name, message.title, message.parent);
+  });
 
-rootRef.on('child_removed', function(snapshot) {
+  rootRef.on('child_removed', function(snapshot) {
     $('#records').find('[data-id="'+snapshot.key()+'"]').remove();
-});
+  });
 
-function displayTitleMessage(id, title, parentId) {
+  function displayTitleMessage(id, name, title, parentId) {
     var $parent = parentId ? findParent(parentId) : $('#records');
-    var $el = makeListItem(title);
-    console.log('displaying', id, parentId, $parent, title);
+    var $el = makeListItem(name, title);
+    console.log('displaying', id, name, parentId, $parent, title);
     // add a data-parent attribute, which we use to locate parent elements
     $el.appendTo($parent).attr('data-id', id);
-}
+  }
 
-function findParent(parentId) {
+  function findParent(parentId) {
     return $('#records').find('[data-id="' + parentId + '"] > ul');
-}
+  }
 
-function makeListItem(title) {
+  function makeListItem(name, title) {
+    console.log('This is name: ' + name);
     return $('#recordTemplate').clone()
     // remove the id attr
     .attr('id', null)
     // enter the <span> tag and use .text() to escape title
-    .find('span').text(title)
+    .find('span').text(name + " : "+ title)
     // navigate back to the cloned element and return it
     .end();
-}
-
+  }
 });
