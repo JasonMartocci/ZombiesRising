065e1c265f2718e28a58f6ae0ed9a00630cb5260
Jason Martocci <Jason Martocci>
2016-07-09 13:10:30 -0400

    working on fullscreen

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,92 +1,145 @@
+// mozfullscreenerror event handler
+function errorHandler() {
+   alert('mozfullscreenerror');
+}
+document.documentElement.addEventListener('mozfullscreenerror', errorHandler, false);
+
+// toggle full screen
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+  $('#fullscreenToggle').hide();
+  // document.location.reload();
+}
+
+// keydown event handler
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode == 13 || e.keyCode == 70) { // F or Enter key
+    toggleFullScreen();
+  }
+}, false);
+
 $(document).ready(function() {
-(function () {
-    var viewFullScreen = document.getElementById("view-fullscreen");
-    if (viewFullScreen) {
-        viewFullScreen.addEventListener("click", function () {
-          $(this).hide();
-            var docElm = document.documentElement;
-            if (docElm.requestFullscreen) {
-                docElm.requestFullscreen();
-            }
-            else if (docElm.msRequestFullscreen) {
-                docElm.msRequestFullscreen();
-            }
-            else if (docElm.mozRequestFullScreen) {
-                docElm.mozRequestFullScreen();
-            }
-            else if (docElm.webkitRequestFullScreen) {
-                docElm.webkitRequestFullScreen();
-            }
-        }, false);
-
-        function hideshow() {
-            document.getElementById('fullscreenButton').style.display = 'block'; 
-            this.style.display = 'none'
-        } 
-    }
-
-    var cancelFullScreen = document.getElementById("cancel-fullscreen");
-    if (cancelFullScreen) {
-        cancelFullScreen.addEventListener("click", function () {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            }
-            else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            }
-            else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            }
-            else if (document.webkitCancelFullScreen) {
-                document.webkitCancelFullScreen();
-            }
-        }, false);
-    }
+  /**
+ *
+ * javascript full-screen window technology - fullscreen mode
+ *
+ * Copyright 2013, Dhiraj kumar
+ * http://www.css-jquery-design.com/
+ */
 
 
-    var fullscreenState = document.getElementById("fullscreen-state");
-    if (fullscreenState) {
-        document.addEventListener("fullscreenchange", function () {
-            fullscreenState.innerHTML = (document.fullscreenElement)? "" : "not ";
-        }, false);
+// (function () {
+//     var viewFullScreen = document.getElementById("view-fullscreen");
+//     if (viewFullScreen) {
+//         viewFullScreen.addEventListener("click", function () {
+//             var docElm = document.documentElement;
+//             if (docElm.requestFullscreen) {
+//                 docElm.requestFullscreen();
+//                 // window.location.reload();
+//                 $('#view-fullscreen').hide();
+//             }
+//             else if (docElm.msRequestFullscreen) {
+//                 docElm.msRequestFullscreen();
+//                 // window.location.reload();
+//                 $('#view-fullscreen').hide();
+//             }
+//             else if (docElm.mozRequestFullScreen) {
+//                 docElm.mozRequestFullScreen();
+//                 // window.location.reload();
+//                 $('#view-fullscreen').hide();
+//             }
+//             else if (docElm.webkitRequestFullScreen) {
+//                 docElm.webkitRequestFullScreen();
+//                 // window.location.reload();
+//                 $('#view-fullscreen').hide();
+//             }
+//         }, false);
+
+//         function hideshow() {
+//             document.getElementById('fullscreenButton').style.display = 'block'; 
+//             this.style.display = 'none'
+//         } 
+//     }
+
+//     var cancelFullScreen = document.getElementById("cancel-fullscreen");
+//     if (cancelFullScreen) {
+//         cancelFullScreen.addEventListener("click", function () {
+//             if (document.exitFullscreen) {
+//                 document.exitFullscreen();
+//             }
+//             else if (document.msExitFullscreen) {
+//                 document.msExitFullscreen();
+//             }
+//             else if (document.mozCancelFullScreen) {
+//                 document.mozCancelFullScreen();
+//             }
+//             else if (document.webkitCancelFullScreen) {
+//                 document.webkitCancelFullScreen();
+//             }
+//         }, false);
+//     }
+
+
+//     var fullscreenState = document.getElementById("fullscreen-state");
+//     if (fullscreenState) {
+//         document.addEventListener("fullscreenchange", function () {
+//             fullscreenState.innerHTML = (document.fullscreenElement)? "" : "not ";
+//         }, false);
         
-        document.addEventListener("msfullscreenchange", function () {
-            fullscreenState.innerHTML = (document.msFullscreenElement)? "" : "not ";
-        }, false);
+//         document.addEventListener("msfullscreenchange", function () {
+//             fullscreenState.innerHTML = (document.msFullscreenElement)? "" : "not ";
+//         }, false);
         
-        document.addEventListener("mozfullscreenchange", function () {
-            fullscreenState.innerHTML = (document.mozFullScreen)? "" : "not ";
-        }, false);
+//         document.addEventListener("mozfullscreenchange", function () {
+//             fullscreenState.innerHTML = (document.mozFullScreen)? "" : "not ";
+//         }, false);
         
-        document.addEventListener("webkitfullscreenchange", function () {
-            fullscreenState.innerHTML = (document.webkitIsFullScreen)? "" : "not ";
-        }, false);
-    }
+//         document.addEventListener("webkitfullscreenchange", function () {
+//             fullscreenState.innerHTML = (document.webkitIsFullScreen)? "" : "not ";
+//         }, false);
+//     }
 
-    var marioVideo = document.getElementById("mario-video")
-        videoFullscreen = document.getElementById("video-fullscreen");
+//     var marioVideo = document.getElementById("mario-video")
+//         videoFullscreen = document.getElementById("video-fullscreen");
 
-    if (marioVideo && videoFullscreen) {
-        videoFullscreen.addEventListener("click", function (evt) {
-            if (marioVideo.requestFullscreen) {
-                marioVideo.requestFullscreen();
-            }
-            else if (marioVideo.msRequestFullscreen) {
-                marioVideo.msRequestFullscreen();
-            }
-            else if (marioVideo.mozRequestFullScreen) {
-                marioVideo.mozRequestFullScreen();
-            }
-            else if (marioVideo.webkitRequestFullScreen) {
-                marioVideo.webkitRequestFullScreen();
-                /*
-                    *Kept here for reference: keyboard support in full screen
-                    * marioVideo.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-                */
-            }
-        }, false);
-    }
-})();
+//     if (marioVideo && videoFullscreen) {
+//         videoFullscreen.addEventListener("click", function (evt) {
+//             if (marioVideo.requestFullscreen) {
+//                 marioVideo.requestFullscreen();
+//             }
+//             else if (marioVideo.msRequestFullscreen) {
+//                 marioVideo.msRequestFullscreen();
+//             }
+//             else if (marioVideo.mozRequestFullScreen) {
+//                 marioVideo.mozRequestFullScreen();
+//             }
+//             else if (marioVideo.webkitRequestFullScreen) {
+//                 marioVideo.webkitRequestFullScreen();
+//                 /*
+//                     *Kept here for reference: keyboard support in full screen
+//                     * marioVideo.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+//                 */
+//             }
+//         }, false);
+//     }
+// })();
 
 
   // DataTables for inventory management
